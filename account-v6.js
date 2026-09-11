fd30027c067a3ca8212b97a44d5a762516cb73a5
OmgKawaiiaAngeI <154029042+OmgKawaiiaAngeI@users.noreply.github.com>
2026-09-11 16:59:13 -0400

    Load profile cosmetics shop

--- account-v6.js
+++ account-v6.js
@@ -2,4 +2,5 @@
   document.write('<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"><\/script>');
   document.write('<script src="account-v9.js?v=1"><\/script>');
   document.write('<script src="sidebar-v9.js?v=1"><\/script>');
+  document.write('<script src="shop-v9.js?v=1"><\/script>');
 })();
