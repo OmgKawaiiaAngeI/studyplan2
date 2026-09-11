9c4b57d086138769c748b1416e7ea12fce0861bb
OmgKawaiiaAngeI <154029042+OmgKawaiiaAngeI@users.noreply.github.com>
2026-09-11 16:58:50 -0400

    Load organized account navigation

--- account-v6.js
+++ account-v6.js
@@ -1,4 +1,5 @@
 (()=>{
   document.write('<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"><\/script>');
   document.write('<script src="account-v9.js?v=1"><\/script>');
+  document.write('<script src="sidebar-v9.js?v=1"><\/script>');
 })();
