e73734954dbab589eec61ed4b56148a5e3c1f33d
OmgKawaiiaAngeI <154029042+OmgKawaiiaAngeI@users.noreply.github.com>
2026-09-11 16:59:29 -0400

    Render equipped profile cosmetics

--- account-v6.js
+++ account-v6.js
@@ -3,4 +3,5 @@
   document.write('<script src="account-v9.js?v=1"><\/script>');
   document.write('<script src="sidebar-v9.js?v=1"><\/script>');
   document.write('<script src="shop-v9.js?v=1"><\/script>');
+  document.write('<script src="cosmetics-render-v9.js?v=1"><\/script>');
 })();
